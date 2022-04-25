68c5c55afaed7c529b283dcc0c0a0e8db1ca9ab4
PlayerUCW <783426Zzz@gmail.com>
2022-04-25 18:03:02 +0300

    deletion test

--- tester2.py
+++ tester2.py
@@ -2,4 +2,12 @@ from requests import get, post, delete
 import datetime
 
 
-print(delete('http://localhost:5000/api/jobs/4').json())
+print(get('http://localhost:5000/api/jobs').json())
+
+
+print(delete('http://localhost:5000/api/jobs/999').json())
+print(delete('http://localhost:5000/api/jobs/-42').json())
+print(delete('http://localhost:5000/api/jobs/3').json())
+
+
+print(get('http://localhost:5000/api/jobs').json())
