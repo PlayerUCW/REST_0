d4cf97a65b893b7c0d41bf91ee5534cc1eb5089a
PlayerUCW <783426Zzz@gmail.com>
2022-04-26 23:44:42 +0300

    edit test

--- tester3.py
+++ tester3.py
@@ -2,6 +2,26 @@ from requests import get, post, delete
 import datetime
 
 
+print(get('http://localhost:5000/api/jobs').json())
+
+print(post('http://localhost:5000/api/jobs/999',json={
+    'team_leader': 1,
+    'job': 'EDITED',
+    'work_size': 5,
+    'collaborators': 1,
+    'start_date': datetime.datetime.strftime(datetime.datetime.now(), format="%m/%d/%Y, %H:%M:%S"),
+    'end_date': datetime.datetime.strftime(datetime.datetime.now(), format="%m/%d/%Y, %H:%M:%S"),
+    'is_finished': True
+}).json())
+print(post('http://localhost:5000/api/jobs/4',json={
+    'team_leader': 42,
+    'job': 'EDITED',
+    'work_size': 5,
+    'collaborators': 1,
+    'start_date': datetime.datetime.strftime(datetime.datetime.now(), format="%m/%d/%Y, %H:%M:%S"),
+    'end_date': datetime.datetime.strftime(datetime.datetime.now(), format="%m/%d/%Y, %H:%M:%S"),
+    'is_finished': True
+}).json())
 print(post('http://localhost:5000/api/jobs/4',json={
     'team_leader': 1,
     'job': 'EDITED',
@@ -11,3 +31,4 @@ print(post('http://localhost:5000/api/jobs/4',json={
     'end_date': datetime.datetime.strftime(datetime.datetime.now(), format="%m/%d/%Y, %H:%M:%S"),
     'is_finished': True
 }).json())
+print(get('http://localhost:5000/api/jobs').json())
